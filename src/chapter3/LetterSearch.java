e9bbf395eba1797c295d88f5e1a8dc578bcb4b02
Mr Ricksan <ricardooliveira.info@gmail.com>
2020-10-07 00:35:55 +0100

    LetterSearch.java

---
+++ src/chapter3/LetterSearch.java
@@ -0,0 +1,28 @@
+package chapter3;
+
+import java.util.Scanner;
+
+public class LetterSearch {
+
+    public static void main(String []args){
+
+        // Get Text
+        System.out.println("Enter some text:");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.next();
+        scanner.close();
+
+        boolean letterFound = false;
+        // Search text for letter A
+        for(int i = 0; i < text.length(); i++) {
+            char currentLetter = text.charAt(i);
+            if (currentLetter == 'A' || currentLetter == 'a') {
+                letterFound = true;
+                break;
+            }
+        }
+
+        if (letterFound) System.out.println("Found!");
+        else  System.out.println("Not Found!");
+    }
+}
